a69f64a161ba7deaa92f1b683b45d2b5d65c9400
suriyasingaravel <suriyasingaravel710.com>
2023-09-27 00:45:21 +0530

    day-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Box } from '@chakra-ui/react'
 import './App.css'
 
+
+
 function App() {
+
+  
  
   return (
     <>
       <div>
       <Box>Jayachandran Srini</Box>
+      
       </div>
     </>
   )
