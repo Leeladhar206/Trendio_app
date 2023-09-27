afaa317bb73491a7fb513049a6455e8f3059adb4
Leeladhar_290 <leeladharjagarapu@gmail.com>
2023-09-27 19:46:27 +0530

    day2-leela

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div style={{paddingTop:"120px"}} >Home</div>
+    <div style={{paddingTop:"120px"}} >
+      <h1>This is our new homepage</h1>
+       </div>
   )
 }
 
