65f1686c2e696c2e3cfade99005e02181cf07c12
suriyasingaravel <suriyasingaravel710.com>
2023-10-01 13:20:16 +0530

    check

---
+++ src/Redux/store.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, legacy_createStore } from 'redux';
+import productReducer from './productReducer/productReducer';
+
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+  productReducer,
+});
+
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+
+
+
+
+
+
