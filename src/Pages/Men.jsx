f676e50d95297507c400d172a04c2a647047f853
shivendra_pratap_dixit <cvendradixit@gmail.com>
2023-10-02 14:54:43 +0530

    Filter and Sorting

--- src/Pages/Men.jsx
+++ src/Pages/Men.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, {useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { getData } from '../Redux/productReducer/action';
 import './Men.css';
 
@@ -13,23 +13,36 @@ import {
   Image,
   Text,
   Skeleton,
+  Select,
 } from '@chakra-ui/react';
 import { FaStar } from 'react-icons/fa';
+import Sidebar from '../Components/Sidebar';
 
 
 
 
 const Men = () => {
+  
+  const dispatch = useDispatch();
   const products = useSelector((store) => store.productReducer.products);
   const loading = useSelector((store) => store.productReducer.loading);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // Dispatch the action to get data
-    dispatch(getData);
-  }, [dispatch]);
-
+ 
+const [searchParams]=useSearchParams()
+const [order,setSort]=useState(searchParams.get("order")||"")
+//console.log(order)
+const paramsobj={
+  params:{
+    brand:searchParams.getAll("brand"),
+    category:searchParams.getAll("category"),
+    material:searchParams.get("material"),
+    color:searchParams.get("color"),
+    _sort:searchParams.get("order") && "price",
+    _order:searchParams.get("order"),
+  }
+}
+ 
+// console.log(paramsobj)
   const menProducts = products.filter(
     (product) => product.gender === 'Men'
   );
@@ -67,12 +80,28 @@ const Men = () => {
 
     return <span className="star-rating">{stars}</span>;
   };
+  useEffect(() => {
+    // Dispatch the action to get data
+    dispatch(getData(paramsobj));
+  }, [searchParams]);
 
+  //position: relative; /* Container needs a position */
+  //height: 100%; /* Set a height to create a scrolling container */
+  // overflow-y: auto;
   return (
-    <Container maxW="container.lg" py={8} pt={20}>
-      <Heading as="h1" mb={4}>
+    <Box maxW="box.lg" display={"flex"} >
+      <Sidebar order={order} setSort={setSort}/>
+    <Container maxW="container.lg" py={8} pt={20} >
+      <Box display={"flex"} justifyContent={"space-between"}>
+      <Heading as="h1" mb={4} >
         Men's Clothing
       </Heading>
+<Select placeholder='Sort By Price' value={order} borderColor='gray' w={["50%","30%","25%"]} onChange={(e)=>setSort(e.target.value)}> 
+  <option value={"asc"}>Low to High</option>
+  <option value={"desc"}>High to Low</option>
+</Select>
+      </Box>
+
       <SimpleGrid columns={[1, 2, 3]} spacing={4}>
         {loading ? (
           Array.from({ length: 6 }).map((_, index) => (
@@ -108,7 +137,7 @@ const Men = () => {
                 <Text>
                  
         <Flex alignItems={"center"} fontSize={16} m={"10px auto"}>
-        Rating:  {new Array(Math.floor(product.rating || 1)).fill(0).map((el, index) => (
+       Rating:  {new Array(Math.floor(product.rating || 1)).fill(0).map((el, index) => (
             <Box key={index} m={"0px 1px"}>
               <FaStar color="#ffb128" />
             </Box>
@@ -131,6 +160,7 @@ const Men = () => {
         )}
       </SimpleGrid>
     </Container>
+    </Box>
   );
 };
 
